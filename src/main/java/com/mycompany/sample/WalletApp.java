f53dba1be9727d8ef47d3fe84afa583b4daee255
TomTomSr <thomas.oberschmied@hotmail.com>
2023-06-16 15:56:59 +0200

    Stand 16.06.2023

--- src/main/java/com/mycompany/sample/WalletApp.java
+++ src/main/java/com/mycompany/sample/WalletApp.java
@@ -1,54 +1,53 @@
 package com.mycompany.sample;
-
 import Exceptions.*;
 import Infrastruktur.CurrentCurrencyPrices;
 import Infrastruktur.FileDataStore;
-import com.gluonhq.attach.display.DisplayService;
-import com.gluonhq.attach.util.Platform;
-import com.gluonhq.charm.glisten.application.AppManager;
-import com.gluonhq.charm.glisten.visual.Swatch;
 import com.mycompany.sample.domain.*;
 import javafx.application.Application;
-import javafx.geometry.Dimension2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ResourceBundle;
 
-import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
+import javafx.fxml.FXMLLoader;
 
-public class Main extends Application {
 
-    public static final String MAIN_VIEW = HOME_VIEW;
+public class WalletApp extends Application {
 
-    private final AppManager appManager = AppManager.initialize(this::postInit);
 
-    @Override
-    public void init() {
-        appManager.addViewFactory(MAIN_VIEW, () -> new MainView().getView());
 
-        DrawerManager.buildDrawer(appManager);
-    }
+    // UI Parts
+    private static Stage mainStage;
 
-    @Override
-    public void start(Stage stage) {
-        appManager.start(stage);
-    }
-
-    private void postInit(Scene scene) {
-        Swatch.LIGHT_GREEN.assignTo(scene);
-        scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
-        ((Stage) scene.getWindow()).getIcons().add(new Image(Main.class.getResourceAsStream("/icon.png")));
-
-        if (Platform.isDesktop()) {
-            Dimension2D dimension2D = DisplayService.create()
-                    .map(DisplayService::getDefaultDimensions)
-                    .orElse(new Dimension2D(640, 480));
-            scene.getWindow().setWidth(dimension2D.getWidth());
-            scene.getWindow().setHeight(dimension2D.getHeight());
+    public static void switchScene(String fxmlFile, String resourceBundle){
+        try {
+            Parent root = FXMLLoader.load(WalletApp.class.getResource(fxmlFile),ResourceBundle.getBundle(resourceBundle));
+            Scene scene = new Scene(root);
+            mainStage.setScene(scene);
+            mainStage.show();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
     }
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        AnchorPane root = FXMLLoader.load(WalletApp.class.getResource("main.fxml"),
+        ResourceBundle.getBundle("com.mycompany.sample.main"));
+        Scene scene =new Scene(root,640,480);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+
+
+
 
     public static void main(String[] args) throws InvalidAmountException, InsufficientAmountException {
 
